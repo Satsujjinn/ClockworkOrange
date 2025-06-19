b7ed9457da47880a6b98e00a270eda2881a392f8
Satsujjinn <186863288+Satsujjinn@users.noreply.github.com>
2025-06-19 15:20:59 +0200

    Convert websocket test to sync

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -1,11 +1,9 @@
-import pytest
 from fastapi.testclient import TestClient
 
 from src.api.main import app
 
 
-@pytest.mark.asyncio
-async def test_websocket_echo():
+def test_websocket_echo():
     with TestClient(app) as client:
         with client.websocket_connect("/ws") as ws:
             ready = ws.receive_json()
